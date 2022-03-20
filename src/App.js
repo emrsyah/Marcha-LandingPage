e6dbf83cacb08b8b991b259ecfa47420895c7b1d
Emirsyah <muhammademir48@gmail.com>
2022-03-20 21:03:38 +0700

    Setting react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <p className='text-3xl font-bold underline'>
-        Ohayo
-      </p>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+        Halo
+    </nav>
+  )
+}
+
+export default Navbar
