8b04777d11343c21bd0362de285c7167841a3695
Emirsyah <muhammademir48@gmail.com>
2022-03-22 22:03:46 +0700

    navigate button, and head changes

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 import Group1 from "../pictures/asked.svg";
+import { useNavigate } from "react-router-dom";
 
 function Contact() {
+  let navigate = useNavigate()
   return (
     <div className="mx-20 my-28 flex bg-purple-600 rounded-2xl">
       <div style={{flexGrow: '3'}} className="flex justify-center flex-col p-8 text-white space-y-4">
@@ -11,7 +13,7 @@ function Contact() {
           Cus langsung aja kirim pertanyaanmu lewat kontak kita, mau apapun itu
           akan kami usahakan balas sebisa mungkin.
         </p>
-        <button className="flex items-center gap-1 bg-white max-w-max px-5 py-3 text-purple-600 font-semibold rounded-lg space-x-4 hover:bg-purple-800 hover:text-white transition-all ease-out duration-200">
+        <button onClick={()=>navigate('/contact')} className="flex items-center gap-1 bg-white max-w-max px-5 py-3 text-purple-600 font-semibold rounded-lg space-x-4 hover:bg-purple-800 hover:text-white transition-all ease-out duration-200">
           <h5>Kontak Sekarang</h5>
           <Icon icon="bytesize:arrow-right" className="font-bold"/>
         </button>
